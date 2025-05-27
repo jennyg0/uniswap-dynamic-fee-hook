5c28724318503e78011d57b9af63cd07a8bd7127
jen <60794961+jennyg0@users.noreply.github.com>
2025-05-27 13:54:22 -0400

    add hook contract

---
+++ src/GasPriceFeeHook.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/types/BeforeSwapDelta.sol";
+import {SwapParams} from "v4-core/types/PoolOperation.sol";
+
+contract GasPriceFeeHook is BaseHook {
+    using LPFeeLibrary for uint24;
+
+    uint128 public movingAverageGasPrice;
+    uint104 public movingAverageGasPriceCount;
+
+    uint24 public constant BASE_FEE = 5000; // 0.5%
+
+    error MustUseDynamicFee();
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {
+        updateMovingAverage();
+    }
+
+    function updateMovingAverage() internal {
+        uint128 gasPrice = uint128(tx.gasprice);
+        // new Average = ((Old Average * # of Txns Tracked) + Current Gas Price) / (# of Txns Tracked + 1)
+        movingAverageGasPrice =
+            ((movingAverageGasPrice * movingAverageGasPriceCount) + gasPrice) /
+            (movingAverageGasPriceCount + 1);
+
+        movingAverageGasPriceCount++;
+    }
+
+    function getFee() internal view returns (uint24) {
+        uint128 gasPrice = uint128(tx.gasprice);
+
+        // if gasPrice > movingAverageGasPrice * 1.1, then half the fees
+        if (gasPrice > (movingAverageGasPrice * 11) / 10) {
+            return BASE_FEE / 2;
+        }
+
+        // if gasPrice < movingAverageGasPrice * 0.9, then double the fees
+        if (gasPrice < (movingAverageGasPrice * 9) / 10) {
+            return BASE_FEE * 2;
+        }
+
+        return BASE_FEE;
+    }
+
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: true,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterAddLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: true,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+
+    function beforeSwap(
+        address,
+        PoolKey calldata key,
+        SwapParams calldata,
+        bytes calldata
+    )
+        external
+        override
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        uint24 fee = getFee();
+
+        uint24 feeWithFlag = fee | LPFeeLibrary.OVERRIDE_FEE_FLAG;
+        return (
+            this.beforeSwap.selector,
+            BeforeSwapDeltaLibrary.ZERO_DELTA,
+            feeWithFlag
+        );
+    }
+
+    function afterSwap(
+        address,
+        PoolKey calldata,
+        SwapParams calldata,
+        BalanceDelta,
+        bytes calldata
+    ) external override returns (bytes4, int128) {
+        updateMovingAverage();
+        return (this.afterSwap.selector, 0);
+    }
+
+    function beforeInitialize(
+        address,
+        PoolKey calldata key,
+        uint160
+    ) external pure override returns (bytes4) {
+        if (!key.fee.isDynamicFee()) revert MustUseDynamicFee();
+        return this.beforeInitialize.selector;
+    }
+}
